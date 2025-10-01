da41a9c03a0f13037deb61a2a13e4b4746919aec
Monique E R Gomes <niquevellin@gmail.com>
2025-10-01 11:41:28 -0300

    correção de métodos e adição de retorno em formato string

--- Tree.py
+++ Tree.py
@@ -10,35 +10,50 @@ class Tree:
     def __init__(self):
         self.root = None
 
-    def insert (self, element):
-        if self.root is None:
-            self.root = TreeNode(element)
-        else:
-            self._recursive_insert(self.root, element)
+    #-----------------------------------------------------------------inserir mensagem em pós ordem na árvore
+    def pos_order_insert(self, message: str):
+        self.root = self._recursive_pos_inset(message)
+        print("Arvore montada")
 
-    def _recursive_insert(self, current, value):
-        if value < current.value:
-            if current.left is None:
-                current.left = TreeNode(value)
-            else:
-                self._recursive_insert(current.left, value)
-        else:
-            if current.right is None:
-                current.right = TreeNode(value)
-            else:
-                self._recursive_insert(current.right, value)
-
-    #---------------------------------percurso em pós ordem
-    def post_order (self, file_name):
-        with open(file_name, "w") as file:
-            self._save_leaves(self.root, file)
-    
-    def _save_leaves(self, node, file):
-        if node is None:
-            return
+    def _recursive_pos_inset(self, partial_message: str):
+        if not partial_message:
+            return None
         
-        self._save_leaves(node.left, file)
-        self._save_leaves(node.right, file)
+        tree_root = TreeNode(partial_message[-1])
+        rest_message = partial_message[:-1]
+        midpoint = len(rest_message) // 2
 
-        if node.left and node.right is None:
-            file.write(str(node.value)+",")
+        tree_root.left = self._recursive_pos_inset(rest_message[:midpoint])
+        tree_root.right = self._recursive_pos_inset(rest_message[midpoint:])
+
+        return tree_root
+
+    #----------------------------------------------percurso em pós ordem salvando em arquivo txt (descriptografia)
+    def pos_order(self, file_name):
+        if self.root is None:
+            return "A árvore está vazia"
+
+        with open(file_name, "w") as file:
+            self._recursive_pos_order(self.root, file)
+
+    def _recursive_pos_order(self, current_node, file):
+        if current_node is None:
+            return
+        self._recursive_pos_order(current_node.left, file)
+        self._recursive_pos_order(current_node.right, file)
+        file.write(str(current_node.value) + ",")
+
+    #---------------------------------------------------pos ordem salvando em lista e retornando uma string (descriptografia)
+    def pos_order_to_string(self):
+        if self.root is None:
+            return "A árvore está vazia"
+        list_result = []
+        self._recursive_pos_order_to_string(self.root, list_result)
+        return "".join(list_result)
+    
+    def _recursive_pos_order_to_string(self, current_node, list_result: list):
+        if current_node is None:
+            return
+        self._recursive_pos_order_to_string(current_node.left, list_result)
+        self._recursive_pos_order_to_string(current_node.right, list_result)
+        list_result.append(current_node.value)
