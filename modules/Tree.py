9849aad8ef224242bed886734dcb023743531a65
Monique E R Gomes <niquevellin@gmail.com>
2025-10-02 15:24:59 -0300

    refatoração e modificação nos métodos da árvore

--- modules/Tree.py
+++ modules/Tree.py
@@ -14,7 +14,16 @@ class Tree:
         self.root = None
     
     #-----------------------------------------------------------------inserir mensagem em pós ordem na árvore
+    # Funcionando e montando a arvore corretamente (recebe mensagem de arquivo txt abero e salvo em uma variável)
     def pos_order_insert(self, message: str):
+        if type(message) is not str:
+            if type(message) is list:
+                new_message = ""
+                for bin in message:
+                    new_message = new_message + bin + " "
+                message = new_message
+            else:
+                return "Erro: tipo de dado inválido. Use string ou lista de strings."
         self.root = self._recursive_pos_inset(message)
         print("Arvore montada")
 
@@ -31,68 +40,72 @@ class Tree:
 
         return tree_root
 
-    #----------------------------------------------percurso em pós ordem salvando em arquivo txt (descriptografia)
-    def pos_order(self, file_name):
-        if self.root is None:
-            return "A árvore está vazia"
-
-        with open(file_name, "w") as file:
-            self._recursive_pos_order(self.root, file)
-
-    def _recursive_pos_order(self, current_node, file):
-        if current_node is None:
-            return
-        self._recursive_pos_order(current_node.left, file)
-        self._recursive_pos_order(current_node.right, file)
-        file.write(str(current_node.value) + ",")
-
-    #---------------------------------------------------pos ordem salvando em lista e retornando uma string (descriptografia)
-    def pos_order_to_string(self):
-        if self.root is None:
-            return "A árvore está vazia"
-        list_result = []
-        self._recursive_pos_order_to_string(self.root, list_result)
-        return "".join(list_result)
-    
-    def _recursive_pos_order_to_string(self, current_node, list_result: list):
-        if current_node is None:
-            return
-        self._recursive_pos_order_to_string(current_node.left, list_result)
-        self._recursive_pos_order_to_string(current_node.right, list_result)
-        list_result.append(current_node.value)
-        
     #---------------------------------------------------pos ordem retornando uma lista (exportar para json)
-        """
-        percorre a árvore em pós ordem e salva os valores em uma lista
-        """
-        
-
-    def post_order_crypto(self):
+    def pos_order_to_list(self):
         if self.root is None:
             return "A árvore está vazia"
         list_result = []
-        self._recursive_post_order_to_list(self.root, list_result)
+        self._recursive_pos_order_to_list(self.root, list_result)
         return list_result
     
-    def _recursive_post_order_to_list(self, current_node, list_result: list):
+    def _recursive_pos_order_to_list(self, current_node, list_result: list):
         if current_node is None:
             return
-        self._recursive_post_order_to_list(current_node.left, list_result)
-        self._recursive_post_order_to_list(current_node.right, list_result)
+        self._recursive_pos_order_to_list(current_node.left, list_result)
+        self._recursive_pos_order_to_list(current_node.right, list_result)
         list_result.append(current_node.value)
-        
-    #---------------------------------------------------inserindo uma lista na árvore (criptografia)
-    def Pos_order_insert(self, values: list):
-        
-        for value in values:
-            self.root = self._insert_recursive(self.root, value)
 
-    def _insert_recursive(self, current_node, value):
-        if current_node is None:
-            return TreeNode(value)
-        if value < current_node.value:
-            current_node.left = self._insert_recursive(current_node.left, value)
-        else:
-            current_node.right = self._insert_recursive(current_node.right, value)
-        return current_node
+    #---------------------------------------------------pos ordem retornando uma string (descriptografia)
+    # String pode ser salva em arquivo externo através de outro módulo/método ou exibida no terminal com print
+    def pos_order_to_string(self):
+        list_tree = self.pos_order_to_list()
+        return "".join(list_tree)
+           
+    #-------------------------------------------------------------------------------pre ordem 
+    # Funcionando - retorna lista de valores em pre ordem
+    def pre_order_to_list(self):
+        if self.root is None:
+            return "A árvore está vazia"
+        list_result = []
+        self._recursive_pre_order(self.root, list_result)
+        return list_result
     
+    def _recursive_pre_order(self, current_node, list_result: list):
+        if current_node is None:
+            return
+        list_result.append(current_node.value)
+        self._recursive_pre_order(current_node.left, list_result)
+        self._recursive_pre_order(current_node.right, list_result)
+
+    #-------------------------------------------------------------------------------pre ordem retornando string (criptografia)
+    # String pode ser salva em arquivo externo através de outro módulo/método ou exibida no terminal com print
+    def pre_order_to_string(self):
+        list_tree = self.pre_order_to_list()
+        return "".join(list_tree)
+    
+    #-----------------------------------------------------------------inserir mensagem em pré ordem na árvore
+    
+    def pre_order_insert(self, message: str):
+        if type(message) is not str:
+            if type(message) is list:
+                new_message = ""
+                for bin in message:
+                    new_message = new_message + bin + " "
+                message = new_message
+            else:
+                return "Erro: tipo de dado inválido. Use string ou lista de strings."
+        self.root = self._recursive_pre_insert(message)
+        print("Arvore montada")
+
+    def _recursive_pre_insert(self, partial_message: str):
+        if not partial_message:
+            return None
+        
+        tree_root = TreeNode(partial_message[0])
+        rest_message = partial_message[1:]
+        midpoint = len(rest_message) // 2
+
+        tree_root.left = self._recursive_pre_insert(rest_message[:midpoint])
+        tree_root.right = self._recursive_pre_insert(rest_message[midpoint:])
+
+        return tree_root
